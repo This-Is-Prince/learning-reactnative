94c1b51b881c755d258ed8376d33cdb8bd998471
This-Is-Prince <this.is.kumar.prince@gmail.com>
2025-06-21 12:54:55 +0530

    feat: new project styler 02

---
+++ hitesh-choudhary/styler02/app/(tabs)/index.tsx
@@ -0,0 +1,11 @@
+import { SafeAreaView, Text, View } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hey!</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
