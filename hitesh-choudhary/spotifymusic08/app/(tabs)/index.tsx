b511f9c07a73631cfd6ca146213d6aea96989c87
This-Is-Prince <this.is.kumar.prince@gmail.com>
2025-06-23 09:07:50 +0530

    feat: spotify music project

---
+++ hitesh-choudhary/spotifymusic08/app/(tabs)/index.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function TabsMainScreen() {
+  return (
+    <View>
+      <Text>TabsMainScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
