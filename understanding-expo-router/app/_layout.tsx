eaa857cc33489fd3edf2b3caf07fce021efb2f22
This-Is-Prince <this.is.kumar.prince@gmail.com>
2025-06-30 10:26:57 +0530

    feat: tailwind setup

--- understanding-expo-router/app/_layout.tsx
+++ understanding-expo-router/app/_layout.tsx
@@ -1,32 +1,13 @@
-// If using Expo Router, import your CSS file in the app/_layout.tsx file
-import '../global.css';
-
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { Stack } from "expo-router";
+import "../global.css"
+import React from "react";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
+    <React.Fragment>
       <StatusBar style="auto" />
-    </ThemeProvider>
+      <Stack />
+    </React.Fragment>
   );
 }
